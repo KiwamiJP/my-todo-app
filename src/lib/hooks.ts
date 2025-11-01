78b5e8771e9777172c1ef408e97c2238aaa9e5e1
KiwamiJP <ssaa.sa549@gmail.com>
2025-11-01 16:42:22 +0630

    fix: update type definitions and add type assertion

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,11 +1,11 @@
 import { useContext } from "react";
 import { TodosContext } from "../context/TodosContextProvider.tsx";
-import { TodoContextType } from "./type.ts";
+import type { TodoContextType } from "./type.ts";
 
 export default function useTodosContext(): TodoContextType {
   const context = useContext(TodosContext);
   if (!context) {
     throw new Error("Forgot to add provider");
   }
-  return context;
+  return context as TodoContextType;
 }
